e1910d6948ec98f5daa200338408fa4b97a0a664
Koshin-Mohamed <koshin.moh@gmail.com>
2019-09-27 17:35:21 +0300

    attach event listeners to each graphic in the what we do section to toggle between the graphic and the information about what they do respectively.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".what-we-do-design").click(function() {
+    $("#show-design").toggle();
+    $("#hide-design").toggle();
+  });
+  $(".what-we-do-dev").click(function() {
+    $("#show-dev").toggle();
+    $("#hide-dev").toggle();
+  });
+  $(".what-we-do-manage").click(function() {
+    $("#show-manage").toggle();
+    $("#hide-manage").toggle();
+  });
+});
