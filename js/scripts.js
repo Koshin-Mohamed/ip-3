2f262f78345ee8c8e4cf460b93254d9026dc3dca
Koshin-Mohamed <koshin.moh@gmail.com>
2019-09-29 02:27:53 +0300

    add overlay, text and border to portfolio section, also added a sumbit button to the form and a white  mouseover effect on the form inputs and submit button.

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,26 @@ $(document).ready(function() {
     $("#show-manage").toggle();
     $("#hide-manage").toggle();
   });
-  
+  $(".work").hover(function() {
+    $("folio, folio p").hover();
+  });
+  $("#name").mouseover(function() {
+    $("#name").css("background-color", "white");
+  });
+  $("#email").mouseover(function() {
+    $("#email").css("background-color", "white");
+  });
+  $("#message").mouseover(function() {
+    $("#message").css("background-color", "white");
+  });
+  $("#submit").mouseover(function() {
+    $("#submit").css("background-color", "white");
+    $("#submit").css("color", "black");
+  });
+  $("#name, #email, #message").val(function(event) {
+    alert(
+      "Success! Message sent. Thanks for contacting us, you'll be hearing from us very soon."
+    );
+    event.preventDefault();
+  });
 });
