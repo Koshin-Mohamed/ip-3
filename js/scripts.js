9a1b64b7b7dea027cc4eb974c3c89b9acc882158
Koshin-Mohamed <koshin.moh@gmail.com>
2019-09-30 10:35:11 +0300

    add jquery mouseover effects to the portfolio section.

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,63 @@ $(document).ready(function() {
     $("#show-manage").toggle();
     $("#hide-manage").toggle();
   });
-  $(".work").hover(function() {
-    $("folio, folio p").hover();
+
+  $("#gray-bg").mouseover(function() {
+    $("#gray-bg-text").show();
   });
+  $("#gray-bg").mouseleave(function() {
+    $("#gray-bg-text").hide();
+  });
+
+  $("#security-logo").mouseover(function() {
+    $("#security-logo-text").show();
+  });
+  $("#security-logo").mouseleave(function() {
+    $("#security-logo-text").hide();
+  });
+
+  $("#geo-brown-bg").mouseover(function() {
+    $("#geo-brown-bg-text").show();
+  });
+  $("#geo-brown-bg").mouseleave(function() {
+    $("#geo-brown-bg-text").hide();
+  });
+
+  $("#dashboard-site").mouseover(function() {
+    $("#dashboard-site-text").show();
+  });
+  $("#dashboard-site").mouseleave(function() {
+    $("#dashboard-site-text").hide();
+  });
+
+  $("#client-site").mouseover(function() {
+    $("#client-site-text").show();
+  });
+  $("#client-site").mouseleave(function() {
+    $("#client-site-text").hide();
+  });
+
+  $("#inputs-site").mouseover(function() {
+    $("#inputs-site-text").show();
+  });
+  $("#inputs-site").mouseleave(function() {
+    $("#inputs-site-text").hide();
+  });
+
+  $("#burned-logo").mouseover(function() {
+    $("#burned-logo-text").show();
+  });
+  $("#burned-logo").mouseleave(function() {
+    $("#burned-logo-text").hide();
+  });
+
+  $("#restaurant-logo").mouseover(function() {
+    $("#restaurant-logo-text").show();
+  });
+  $("#restaurant-logo").mouseleave(function() {
+    $("#restaurant-logo-text").hide();
+  });
+
   $("#name").mouseover(function() {
     $("#name").css("background-color", "white");
   });
