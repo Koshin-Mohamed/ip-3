00fdd9d16be8f3d5946fa726ee9d72744ad8397d
Koshin-Mohamed <koshin.moh@gmail.com>
2019-09-30 09:43:33 +0300

    add styling to form and mailchimp integration as well as overlays to the portfolio.

--- js/scripts.js
+++ js/scripts.js
@@ -27,10 +27,47 @@ $(document).ready(function() {
     $("#submit").css("background-color", "white");
     $("#submit").css("color", "black");
   });
-  $("#name, #email, #message").val(function(event) {
-    alert(
-      "Success! Message sent. Thanks for contacting us, you'll be hearing from us very soon."
-    );
+  // $("#form").submit(function(event) {
+  //   $("#name").val();
+  //   $("#email").val();
+  //   $("#message").val();
+  //   alert(
+  //     "Success! Message sent. Thanks for contacting us, you'll be hearing from us very soon."
+  //   );
+  //   event.preventDefault();
+  // });
+
+  $("#form").bind("submit", function(event) {
     event.preventDefault();
+    var name = $('[name="b_name"]').val();
+    var email = $('[name="b_email"]').val();
+    var message = $('[name="b_message"]').val();
+    var url =
+      "https://u20.api.mailchimp.com/2.0/lists/subscribe.json?" +
+      "apikey=64a0400834ed78d8a8e15919a870931a-us20&id=2b12f9ee69";
+    "&email[email]=" +
+      email +
+      "&merge_vars[FNAME]=" +
+      name +
+      "&merge_vars[MESSAGE]=" +
+      message +
+      "&double_optin=true" +
+      "&send_welcome=true";
+    $.ajax({
+      type: "POST",
+      url: url,
+      dataType: "json",
+      success: function(data) {
+        $("#form").html(
+          "Success " +
+            name +
+            "! Message sent. Thanks for contacting us, you'll be hearing from us very soon."
+        );
+        alert("success");
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert(errorThrown);
+      }
+    });
   });
 });
